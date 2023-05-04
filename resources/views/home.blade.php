bfe2597a8c8308d93a8d7edfecaae167cef7d213
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-05-04 18:25:26 +0200

    sistemate cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,21 +8,30 @@
     </head>
     <body>
 
-        <h1>MOVIES</h1>
+        <h1 class="text-center py-3">MOVIES</h1>
        
-        <div class="contain">
-            <div class="card flex-row" style="width: 18rem;">
+        <div class="contain d-flex flex-row flex-wrap col-8 gap-3 mx-auto">
+            {{-- <div class="card flex-row" style="width: 18rem;"> --}}
                 @foreach ($movies as $movie )
-                    <ul class="list-group ">
+                    {{-- <ul class="list-group ">
                         <li class="list-group-item">Title: {{ $movie->title }}</li>
                         <li class="list-group-item">Original title: {{ $movie->original_title }}</li>
                         <li class="list-group-item">Nationality: {{ $movie->nationality }}</li>
                         <li class="list-group-item">Release date: {{ $movie->date }}</li>
                         <li class="list-group-item">Vote :{{ $movie->vote }}</li>
-                    </ul>
+                    </ul> --}}
+                    <div class="card" style="width: 18rem;">
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">Title: {{ $movie->title }}</li>
+                            <li class="list-group-item">Original title: {{ $movie->original_title }}</li>
+                            <li class="list-group-item">Nationality: {{ $movie->nationality }}</li>
+                            <li class="list-group-item">Release date: {{ $movie->date }}</li>
+                            <li class="list-group-item">Vote :{{ $movie->vote }}</li>
+                        </ul>
+                    </div>
                     
                 @endforeach
-            </div>
+            {{-- </div> --}}
         </div>
     </body>
 </html>
