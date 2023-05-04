caabc75af50809bf8a56c5563afe7bacfe0e3667
MattiaFezzardi <mattiafezzardi1993@gmail.com>
2023-05-04 16:45:59 +0200

    creati controller e model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,10 +7,7 @@
          @vite('resources/js/app.js')
     </head>
     <body>
-        <div class="container">
-            <h1>Template Classe 89</h1>
-            <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="c" srcset="">
-        </div>
+       <h1>welcome</h1>
 
     </body>
 </html>
